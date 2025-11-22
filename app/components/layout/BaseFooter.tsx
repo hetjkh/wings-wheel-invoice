b4938af77bd30437287df5266ab4bdc60b8f2a36
hetjkh <119274796+hetjkh@users.noreply.github.com>
2025-11-22 15:29:06 +0530

    wefw

--- app/components/layout/BaseFooter.tsx
+++ app/components/layout/BaseFooter.tsx
@@ -1,25 +1,8 @@
 "use client";
 
-import { useTranslationContext } from "@/contexts/TranslationContext";
-
-// Variables
-import { AUTHOR_GITHUB } from "@/lib/variables";
-
 const BaseFooter = () => {
-    const { _t } = useTranslationContext();
-
     return (
         <footer className="container py-10">
-            <p>
-                {_t("footer.developedBy")}{" "}
-                <a
-                    href={AUTHOR_GITHUB}
-                    target="_blank"
-                    style={{ textDecoration: "underline" }}
-                >
-                    Ali Abbasov
-                </a>
-            </p>
         </footer>
     );
 };
